c95ee229218848aa9e0be52047c74377fde72508
wangwei <3511622339@qq.com>
2017-08-22 09:54:05 +0800

    切字数据提交操作

---
+++ core/urls.py
@@ -0,0 +1,21 @@
+from rest_framework import routers
+from views import CutBatchOPViewSet, BatchVersionViewSet, PageViewSet
+from django.conf.urls import url, include
+from rest_framework.schemas import get_schema_view
+from rest_framework_swagger.renderers import OpenAPIRenderer, SwaggerUIRenderer
+
+router = routers.SimpleRouter(trailing_slash=False)
+router.register(r'cutbatch', CutBatchOPViewSet)
+router.register(r'batchversion', BatchVersionViewSet)
+router.register(r'page', PageViewSet)
+
+
+schema_view = get_schema_view(
+    title='Example API',
+    renderer_classes=[OpenAPIRenderer, SwaggerUIRenderer]
+)
+
+urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^swagger/$', schema_view)
+]
