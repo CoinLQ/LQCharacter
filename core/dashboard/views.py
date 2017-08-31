18f0a2d4340da2df79841010cf40d1150f80e334
wangwei <3511622339@qq.com>
2017-08-31 09:34:59 +0800

    index页修改，增加API页面跳转链接

--- core/dashboard/views.py
+++ core/dashboard/views.py
@@ -1,5 +1,9 @@
 from django.views.generic import ListView, CreateView, \
     UpdateView, DeleteView, TemplateView, DetailView
+from django.shortcuts import render
 
 class IndexView(TemplateView):
     template_name = "dashboard/index.html"
+
+def detail(request, page_id):
+    return render(request,"dashboard/index.html",{'page_id': page_id})
