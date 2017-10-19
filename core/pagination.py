31295b2513d2e067a48a5c4c8eb5ff3106219261
xiandian <lvpython@gmail.com>
2017-10-19 16:07:58 +0800

    更新使用materialize

--- core/pagination.py
+++ core/pagination.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 
 
 class StandardPagination(pagination.PageNumberPagination):
-    page_size = 10
+    page_size = 12
     page_size_query_param = 'page_size'
     max_page_size = 10000
 
